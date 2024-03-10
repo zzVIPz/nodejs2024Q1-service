4919e697ea134a6b6c2d45282ec7438bd76ad9ad
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-03-10 17:33:33 -0400

    feat: read port from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
