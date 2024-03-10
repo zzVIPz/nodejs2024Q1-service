ab7335d7afaa8f542946d38e15b7791ebd920f9d
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-03-10 17:37:08 -0400

    feat: add mocked user

---
+++ src/mock-data/mock-data.ts
@@ -0,0 +1,8 @@
+export const mockedUser = {
+  id: 'ecac5ac3-2aa4-4b36-a151-f4f4ecfade8e',
+  login: 'test-user',
+  version: 1,
+  createdAt: 1710092385020,
+  updatedAt: 1710092385020,
+  password: 'test-user',
+};
