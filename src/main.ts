7ce1a9d145d209b853e5bb2d95486348c17d53e2
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-03-11 18:46:02 -0400

    feat: update project dependencies to add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('Home Library Service').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 4000);
 }
